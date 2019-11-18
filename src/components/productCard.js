96529fca7d079c1ca99ffb32e9a8ea280c532cb2
SrTokyo <52584498+SrTokyo@users.noreply.github.com>
2019-11-18 18:00:29 -0500

    Test New Cards

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -4,7 +4,32 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
 
 function ProductCard({ pTitle }){
+    var product = {
+        nombre: 'Nombre del producto',
+        Descripcion: 'Descripcion del producto',
+        imagen : './images/carro1.jpg',
+        getNombre: function() {
+          return this.nombre;
+        },
+        getDescripcion: function() {
+            return this.Descripcion;
+        },
+        getImagen: function() {
+            return this.imagen;
+        }
+    };
     return (
+        <section>
+          <img src={img1} alt='image1' width="300px" height ="150px"/>
+          <section width = "300px">
+          <h6 align = "center" >{pTitle}</h6><p> Precio : 300</p>
+          </section>
+          <section width = "300px">
+          <p align = "center">Descripcion del producto</p>
+          </section>
+        </section>
+    )
+    /*return (
         <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" />
             <Card.Body>
@@ -17,6 +42,7 @@ function ProductCard({ pTitle }){
             </Card.Body>
         </Card>
     );  
+    */
 }
 
 export default ProductCard
