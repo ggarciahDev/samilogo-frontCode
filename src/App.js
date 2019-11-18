bee9c1791d4a39f470e11000b4814646cf210f3e
Camilo Gaviria <cgavir29@eafit.edu.co>
2019-11-18 17:18:52 -0500

    Fixed Bug

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from './Components/login';
-import Signup from './Components/signup';
-import listProducts from './Components/listProducts';
+import Login from './components/login';
+import Signup from './components/signup';
+import listProducts from './components/listProducts';
 
 function App() {
   return (
