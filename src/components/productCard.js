6de81c03fb7d505dab74cea3f2578331c3edb3e7
Gonzalo Garcia <ggarciah@eafit.edu.co>
2019-11-20 18:58:17 -0500

    pID de productCard.js

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom'
 
-function ProductCard({ pTitle, pDescription, pId }) {
+function ProductCard({ pTitle, pDescription, pID }) {
     return (
         <Card style={{ minWidth: '18rem', minheigth: "250px", maxwidth: '18rem', maxheigth: "250px" }} >
             <Card.Img variant="top" src="http://pictures.dealer.com/h/hoehnaudiaoa/1974/39364e173c4a45b2fee2724ec7310f30x.jpg" />
@@ -13,8 +13,8 @@ function ProductCard({ pTitle, pDescription, pId }) {
                     {pDescription}
                 </Card.Text>
 
-                <Link className="Font-Link" to={"/"+pId}>
-                    <Button variant="primary">Comments X</Button>
+                <Link className="Font-Link" to={"/"+pID}>
+                    <Button variant="primary">Comments</Button>
                 </Link>
             </Card.Body>
         </Card>
