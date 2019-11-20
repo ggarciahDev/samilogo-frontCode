ed0f5cf2a0410f7c6e22771416a93e5cad253e2c
Gonzalo Garcia <ggarciah@eafit.edu.co>
2019-11-20 17:52:04 -0500

    Ojala de

---
+++ src/components/listComments.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ListComments extends Component {
+    render() {
+        return (
+            <div className="App">
+                <segment className="BackColor">
+                    <h4>Buena esa, compañero ~(*o*)~</h4>
+                </segment>
+            </div>
+        )
+    }
+}
+
+
+export default ListComments
+
