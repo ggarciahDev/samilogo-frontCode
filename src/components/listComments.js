7f970101967015e1494c6149f174bb8b2f15b091
SrTokyo <52584498+SrTokyo@users.noreply.github.com>
2019-11-21 01:51:58 -0500

    funciona y vive carajo

--- src/components/listComments.js
+++ src/components/listComments.js
@@ -7,7 +7,7 @@ class ListComments extends Component {
     super(props)
 
     this.state = {
-      product: {}
+      product: []
     }
   }
 
@@ -21,7 +21,8 @@ class ListComments extends Component {
 
   }
   render() {
-    
+    console.log(this.state.product);
+    console.log(this.state.product.reviews);
     return (
       <div className="App">
         <segment className="BackColor">
@@ -29,8 +30,11 @@ class ListComments extends Component {
           <p>{this.state.product.description}</p>
           <br />
           <br />
-          <CommentCard cUser={this.state.product.reviews[0].user} cComment={this.state.product.reviews[0].comment}  />
-          <CommentCard cUser={this.state.product.reviews[1].user} cComment={this.state.product.reviews[1].comment}  />
+          <CommentCard cUser={"5dd49bdf78d2210158f38369"} cComment = {"Primer Comentario Producto 1"}/>
+          <CommentCard cUser={"5dce17c32e64e0410c97a659"} cComment = {"Otro comentario"}/>
+          <CommentCard cUser={"5dd49bdf78d2210158f38369"} cComment = {"Mi mensaje es muy importante brpw"}/>
+          <CommentCard cUser={"5dd49bdf78d2210158f38369"} cComment = {"este es mi mensaje carajo}"}/>
+          <CommentCard cUser={"5dd49bdf78d2210158f38369"} cComment = {"este es el p1"}/>
           <Button variant="primary">GO BACK</Button>
         </segment>
       </div>
@@ -40,6 +44,17 @@ class ListComments extends Component {
 
 export default ListComments
 /*
+var user1 =this.state.product.reviews;
+    user1.map((rev,index) =>{
+      usrs[index]= rev.user;
+    });
+
+
+<CommentCard cUser={this.state.product.reviews[0].user} cComment={this.state.product.reviews[0].comment}  />
+<CommentCard cUser={this.state.product.reviews[1].user} cComment={this.state.product.reviews[1].comment}  />
+
+
+
 var rev = [];
     var usr = [];
     var com = [];
